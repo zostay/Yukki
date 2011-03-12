67d5fba54a0edc5e4c6d80fbfc2ea89b43a5cd0b
Sterling Hanenkamp <hanenkamp@cpan.org>
2011-03-12 08:01:39 -0600

    Added tree support to the page model
    You can now add/pages/with/full/paths.
    
    Also changed the default file suffix to .yukki and made it so that you can
    specify the filetype suffix.

--- lib/Yukki/Model/Page.pm
+++ lib/Yukki/Model/Page.pm
@@ -10,21 +10,80 @@ sub repository_settings {
     return $self->app->settings->{repositories}{$repository};
 }
 
-sub save {
-    my ($self, $repository, $page, $params) = @_;
-
-    $page .= '.mkd';
+sub git {
+    my ($self, $repository) = @_;
 
     my $repo_settings = $self->repository_settings($repository);
     return unless defined $repo_settings;
 
     my $repo_dir = $self->locate('repository_path', $repo_settings->{repository});
-    my $git = Git::Repository->new( git_dir => $repo_dir );
+    return Git::Repository->new( git_dir => $repo_dir );
+}
 
-    my $object_id = $git->run('hash-object', '-t', 'blob', '-w', '--stdin', '--path', $page, 
-        { input => $params->{content} });
+sub make_tree {
+    my ($self, $git, $base, $tree, $blob) = @_;
+    my @tree = @$tree;
 
-    Yukki::Error->throw("unable to create blob for $page") unless $object_id;
+    my ($mode, $type, $name);
+    if (@$tree == 1) {
+        $mode = '100644';
+        $type = 'blob';
+    }
+    else {
+        $mode = '040000';
+        $type = 'tree';
+    }
+    $name = shift @tree;
+
+    my $overwrite;
+    my @new_tree;
+    if (defined $base) {
+        my @old_tree = $git->run('ls-tree', $base);
+        for my $line (@old_tree) {
+            my ($old_mode, $old_type, $old_object_id, $old_page) = split /\s+/, $line;
+
+            if ($old_page eq $name) {
+                $overwrite++;
+
+                Yukki::Error->throw("cannot replace $old_type $name with $type")
+                    if $old_type ne $type;
+
+                if ($type eq 'blob') {
+                    push @new_tree, "$mode $type $blob\t$name";
+                }
+                else {
+                    my $tree_id = $self->make_tree($git, $old_object_id, \@tree, $blob);
+                    push @new_tree, "$mode $type $tree_id\t$name";
+                }
+            }
+            else {
+                push @new_tree, $line;
+            }
+        }
+    }
+    
+    unless ($overwrite) {
+        if ($type eq 'blob') {
+            push @new_tree, "$mode $type $blob\t$name";
+        }
+        else {
+            my $tree_id = $self->make_tree($git, undef, \@tree, $blob);
+            push @new_tree, "$mode $type $tree_id\t$name";
+        }
+    }
+
+    return $git->run('mktree', { input => join "\n", @new_tree });
+}
+
+sub make_blob {
+    my ($self, $git, $name, $content) = @_;
+
+    return $git->run('hash-object', '-t', 'blob', '-w', '--stdin', '--path', $name, 
+        { input => $content });
+}
+
+sub find_root {
+    my ($self, $git) = @_;
 
     my $old_tree_id;
     my @ref_info = $git->run('show-ref', 'refs/heads/master');
@@ -37,28 +96,28 @@ sub save {
         }
     }
 
+    return $old_tree_id;
+}
+
+sub save {
+    my ($self, $repository, $page, $params) = @_;
+
+    $page .= '.' . ($params->{filetype} || 'yukki');
+
+    my $git = $self->git($repository);
+    return unless $git;
+
+    my (@parts) = split m{/}, $page;
+    my $blob_name = $parts[-1];
+
+    my $object_id = $self->make_blob($git, $blob_name, $params->{content});
+    Yukki::Error->throw("unable to create blob for $page") unless $object_id;
+
+    my $old_tree_id = $self->find_root($git);
     Yukki::Error->throw("unable to locate original tree ID for refs/heads/master")
         unless $old_tree_id;
 
-    my $overwrite;
-    my @new_tree;
-    my @old_tree = $git->run('ls-tree', 'refs/heads/master');
-    for my $blob (@old_tree) {
-        my ($old_mode, $old_type, $old_object_id, $old_page) = split /\s+/, $blob;
-
-        if ($old_page eq $page) {
-            $overwrite++;
-            push @new_tree, "$old_mode $old_type $object_id\t$page";
-        }
-        else {
-            push @new_tree, $blob;
-        }
-    }
-    
-    push @new_tree, "100644 blob $object_id\t$page" unless $overwrite;
-
-    my $new_tree_id = $git->run('mktree', { input => join "\n", @new_tree });
-
+    my $new_tree_id = $self->make_tree($git, $old_tree_id, \@parts, $object_id);
     Yukki::Error->throw("unable to create the new tree containing $page\n")
         unless $new_tree_id;
 
